f4f4d7e841b6b07646d8e9c4d4434b53b43c1d88
Nico <125397197+mewmewmewmewmewmewmew@users.noreply.github.com>
2024-09-13 08:43:58 -0400

    Update PicCrewGenerator.cs

--- Assets/PicCrewGenerator.cs
+++ Assets/PicCrewGenerator.cs
@@ -8,15 +8,10 @@ public class PicCrewGenerator : MonoBehaviour
     // Start is called before the first frame update
 
     public List<Sprite> ClothesAttributes = new List<Sprite>();
-    Sprite CurrentClothesAttribute;
     public List<Sprite> EyeAttributes = new List<Sprite>();
-    Sprite CurrentEyeAttribute;
     public List<Sprite> HairAttributes = new List<Sprite>();
-    Sprite CurrentHairAttribute;
     public List<Sprite> MouthAttributes = new List<Sprite>();
-    Sprite CurrentMouthAtrribute;
     public List<Sprite> BaseAttributes= new List<Sprite>();
-    Sprite CurrentBaseAttribute;
 
     public Image Clothes;
     public Image Eye;
@@ -33,25 +28,20 @@ public class PicCrewGenerator : MonoBehaviour
         //5
         images.Add(Clothes);  images.Add(Eye); images.Add(Hair); images.Add (Base); images.Add(Mouth);
         //5
-        AllAttributes.Add(CurrentClothesAttribute); AllAttributes.Add(CurrentEyeAttribute); AllAttributes.Add(CurrentHairAttribute); AllAttributes.Add(CurrentBaseAttribute); AllAttributes.Add(CurrentMouthAtrribute);
-        //5
         AllAttributeLists.Add(ClothesAttributes); AllAttributeLists.Add(EyeAttributes); AllAttributeLists.Add(HairAttributes); AllAttributeLists.Add(BaseAttributes); AllAttributeLists.Add(MouthAttributes);
         RandomizeAllPicCrewAttributes();
     }
 
     void RandomizePicCrewAttribute( List<Sprite> myAttributeList, Image image)
     {
-
         image.sprite = myAttributeList[Random.Range(0, myAttributeList.Count)];
         Debug.Log(image.sprite + myAttributeList[0].name);
-
     }
 
     public void RandomizeAllPicCrewAttributes()
     {
         for (int i = 0; i < AllAttributeLists.Count; i++)
         {
-
             RandomizePicCrewAttribute(AllAttributeLists[i], images[i]);
         }
     }
